68e9fe06bc6ba51e7401590aa3a0f58fdb2eb431
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-04-28 17:52:35 -0500

    Upload to S3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const asciify = require('asciify-image')
 const fs = require('fs')
+const { S3 } = require('aws-sdk')
  
 const options = {
   fit:    'box',
@@ -16,12 +17,23 @@ const asciifyImg = async (image) => {
   }
 } 
 
-// asciifyImg()
-
 exports.handler = async (event) => {
-  const image = event.image
+  const { image, fileName } = event
   const base64Data = image.replace(/^data:image\/png;base64,/, '')
-  const binaryData = new Buffer(base64Data, 'base64').toString('binary')
-  fs.writeFileSync('out.jpg', binaryData)
-  return await asciifyImg('out.jpg')
+  const buffer = new Buffer(base64Data, 'base64')
+  const params = {
+    Bucket: 'upload.asciify.galaxiaskyklos.com',
+    Key: fileName,
+    Body: buffer,
+    Region: 'us-east-2'
+  }
+  const s3 = new S3()
+  try {
+    await s3.putObject(params).promise()
+    const result = await asciifyImg(`https://s3-us-east-2.amazonaws.com/${fileName}`)
+    await s3.deleteObject(params).promise()
+    return result
+  } catch (e) {
+    return e
+  }
 }
