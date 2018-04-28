a77d7a7fff17f6cb9f8d0d38055347b45f5aa7db
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-04-28 18:17:51 -0500

    Update replace

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ exports.handler = async (event) => {
   const buffer = new Buffer(base64Data, 'base64').toString()
   const params = {
     Bucket: 'upload.asciify.galaxiaskyklos.com',
-    Key: fileName,
+    Key: fileName.replace(' ', ''),
     Body: buffer,
     ACL: 'public-read',
   }
   const s3 = new S3()
   try {
     await s3.putObject(params).promise()
-    const result = await asciifyImg(`https://s3.us-east-2.amazonaws.com/${params.Bucket}/${fileName}`)
+    const result = await asciifyImg(`https://s3.us-east-2.amazonaws.com/${params.Bucket}/${params.Key}`)
     // delete params['Body']
     // delete params['ACL']
     // await s3.deleteObject(params).promise()
