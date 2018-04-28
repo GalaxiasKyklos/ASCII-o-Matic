e6868d530e0958a2dc3e45c697f7b75783c772f5
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-04-28 17:18:43 -0500

    setup project

---
+++ index.js
@@ -0,0 +1,22 @@
+// const asciify = require('asciify-image')
+ 
+// const options = {
+//   fit:    'box',
+//   width:  100,
+//   height: 40,
+//   color: false,
+// }
+
+// const asciifyImg = async (image) => {
+//   try {
+//     console.log(await asciify('./logo.jpg', options))
+//   } catch (e) {
+//     console.error(e)
+//   }
+// } 
+
+// asciifyImg()
+
+exports.handler = async (event) => {
+  return event;
+};
