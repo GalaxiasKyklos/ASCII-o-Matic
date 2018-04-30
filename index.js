9c5099c49e3d685be139433bde117ebb6141b9ef
Saúl Ponce <saul_enrique-7285-96@hotmail.com>
2018-04-30 11:10:47 -0500

    URL support and refactor

--- index.js
+++ index.js
@@ -4,44 +4,75 @@ const {
   S3
 } = require('aws-sdk')
 
-const options = {
-  fit: 'box',
-  width: 100,
-  height: 40,
-  color: false,
-}
-
-const asciifyImg = async (image) => {
+const asciifyImg = async (image, width = 100, height = 100) => {
   try {
+    const options = {
+      color: false,
+      fit: 'box',
+      height,
+      width,
+    }
     return await asciify(image, options)
   } catch (e) {
     return e
   }
 }
 
-exports.handler = async (event) => {
-  const {
-    image,
-    fileName
-  } = event
-  const base64Data = image
-    .replace('data:image/png;base64,', '')
-    .replace('data:image/jpg;base64,', '')
-    .replace('data:image/jpeg;base64,', '')
-  const buffer = new Buffer(base64Data, 'base64')
+const uploadToS3 = async (s3, fileName, buffer) => {
   const params = {
     Bucket: 'upload.asciify.galaxiaskyklos.com',
     Key: fileName.replace(' ', ''),
     Body: buffer,
     ACL: 'public-read',
   }
-  const s3 = new S3()
+  await s3.putObject(params).promise()
+  return {
+    key: params.Key,
+    bucket: params.Bucket,
+  }
+}
+
+const deleteFromS3 = async (s3, fileName) => {
+  const params = {
+    Bucket: 'upload.asciify.galaxiaskyklos.com',
+    Key: fileName.replace(' ', ''),
+  }
+  await s3.deleteObject(params).promise()
+}
+
+const getBuffer = (image) => {
+  const base64Data = image
+    .replace('data:image/png;base64,', '')
+    .replace('data:image/jpg;base64,', '')
+    .replace('data:image/jpeg;base64,', '')
+  const buffer = new Buffer(base64Data, 'base64')
+  return buffer
+}
+
+exports.handler = async (event) => {
+  const {
+    image,
+    fileName,
+    options: {
+      width,
+      height,
+    },
+    url,
+  } = event
   try {
-    await s3.putObject(params).promise()
-    const result = await asciifyImg(`https://s3.us-east-2.amazonaws.com/${params.Bucket}/${params.Key}`)
-    delete params['Body']
-    delete params['ACL']
-    await s3.deleteObject(params).promise()
+    let imgUrl
+    let s3
+    if (url) {
+      imgUrl = url
+    } else {
+      s3 = new S3()
+      const s3Info = await uploadToS3(s3, fileName, getBuffer(image))
+      imgUrl = `https://s3.us-east-2.amazonaws.com/${s3Info.bucket}/${s3Info.key}`
+    }
+    const result = await asciifyImg(imgUrl, width, height)
+    if (!url) {
+      await deleteFromS3(s3, fileName)
+    }
     return result
   } catch (e) {
     return e
